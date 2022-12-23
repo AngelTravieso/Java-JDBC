1407aab44a17e46c074e4fb1b7bb2bf620609d7a
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-23 11:56:58 -0400

    procesando el resultado (resulset)

--- IntroduccionJDBC/src/main/java/test/TestMySqlJDBC.java
+++ IntroduccionJDBC/src/main/java/test/TestMySqlJDBC.java
@@ -6,12 +6,12 @@ public class TestMySqlJDBC {
 
     public static void main(String[] args) {
         // 1. Cadena de conexion para conectarnos a MYSQL
-        String url = "jdbc:mysql://localhost:3306/test_java?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String url = "jdbc:mysql://localhost:3306/test_java?&useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
         // 2. Conexion desde java con JDBC
         try {
             // Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conexion = DriverManager.getConnection(url, "root", "admin");
+            Connection conexion = DriverManager.getConnection(url, "root", "");
 
             /*
             crear objeto de tipo statement, para poder ejecutar sentencias
@@ -24,6 +24,31 @@ public class TestMySqlJDBC {
 
             // Ejecutar instructtion
             ResultSet resultado = instruccion.executeQuery(sql);
+
+            /*
+            como puede devolver mas de un registro se debe iterar el
+            resultado usando un ciclo while
+             */
+            // si hay elementos para iterar
+            while (resultado.next()) {
+                // recuperar columna
+                System.out.print("Id Persona: " + resultado.getInt("persona_id") + ",");
+                System.out.print(" Nombre: " + resultado.getString("nombre") + " ,");
+                System.out.print(" Apellido: " + resultado.getString("apellido") + ",");
+                
+                System.out.print(" Email: " + resultado.getString("email") + ",");
+                
+                System.out.print(" Telefono: " + resultado.getString("telefono") + ",");
+                
+                System.out.println("");
+
+            }
+            
+            // cerrar objetos de conexion
+            resultado.close();
+            instruccion.close();
+            conexion.close();
+
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
