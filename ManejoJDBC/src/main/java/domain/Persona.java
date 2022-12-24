c80298ee58dac37b01fceef2a2d9af4863aa7c13
AngelTravieso <angeltraviesoc@gmail.com>
2022-12-23 21:37:35 -0400

    entidad (clase) Persona
    clase para manejar conexiones

---
+++ ManejoJDBC/src/main/java/domain/Persona.java
@@ -0,0 +1,80 @@
+package domain;
+
+// clase entidad tabla Persona
+public class Persona {
+
+    private int personaId;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public Persona() {
+
+    }
+
+    public Persona(int personaId) {
+        this.personaId = personaId;
+    }
+
+    public Persona(String nombre, String apellido, String email, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public Persona(int personaId, String nombre, String apellido, String email, String telefono) {
+        this.personaId = personaId;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public int getPersonaId() {
+        return personaId;
+    }
+
+    public void setPersonaId(int personaId) {
+        this.personaId = personaId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "personaId=" + personaId + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", telefono=" + telefono + '}';
+    }
+
+}
